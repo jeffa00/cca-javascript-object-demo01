1d7165871d59db5d9a31b48f6744969b6f150ca4
jeffa00 <jeffa00@gmail.com>
2016-12-13 21:20:36 -0500

    2 Customers from Constructor

--- js/objectsAndArrays.js
+++ js/objectsAndArrays.js
@@ -2,19 +2,29 @@ var greetButton = document.getElementById("goButton"),
     displayText = document.getElementById("displayText"),
     personToGreet = "";
 
-var customer1 = {
-    firstName: "Jeff",
-    lastName: "Ammons"
-},
-customer2 = {
-    firstName: "Sue",
-    lastName: "Smith"
+var Customer = function Customer(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = "";
+    this.address1 = "";
+    this.address2 = "";
+    this.city = "";
+    this.state = "";
+    this.zip = "";
+    this.greetFormally = function greetFormally(){
+        return "Greetings, Mr. or Ms. " + this.lastName;
+    }
 };
+
+var customer1 = new Customer("Jeff", "Ammons"),
+    customer2 = new Customer("Sue", "Smith");
+
+    customer1.email = "jeffa00@gmail.com";
 
 greetButton.addEventListener("click", function(){
 
-    var peopleToGreet = customer1.firstName + " " + customer1.lastName + "<br>";
-    peopleToGreet += customer2.firstName + " " + customer2.lastName + "<br>";
+    var peopleToGreet = customer1.firstName + " " + customer1.lastName + " " + customer1.greetFormally() + "<br>";
+    peopleToGreet += customer2.firstName + " " + customer2.lastName + " " + customer2.greetFormally() +  "<br>";
 
     displayText.innerHTML = peopleToGreet;
 });
